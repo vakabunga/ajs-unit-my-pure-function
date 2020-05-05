b72f877c7dd377978fc139186b95433f66c64adb
Vakabunga <shesterov@gmail.com>
2020-05-05 22:18:02 +0300

    add test describe; add .appveyor.yml

--- src/js/__tests__/healthState.test.js
+++ src/js/__tests__/healthState.test.js
@@ -1,22 +1,24 @@
 import healthState from '../app';
 
-test('healthState: should return "healthy" if current health from 51 to 100', () => {
-  const result = healthState({ name: 'Маг', health: 90 });
-  expect(result).toBe('healthy');
-});
-test('healthState: should return "wounded" if current health from 15 to 50', () => {
-  const result = healthState({ name: 'лучник', health: 45 });
-  expect(result).toBe('wounded');
-});
-test('healthState: should return "critical" if current health from 0 to 14', () => {
-  const result = healthState({ name: 'Рыцарь', health: 14 });
-  expect(result).toBe('critical');
-});
-test('healthState: should return "wounded" if current health is 50', () => {
-  const result = healthState({ name: 'Рыцарь', health: 50 });
-  expect(result).toBe('wounded');
-});
-test('healthState: should return "wounded" if current health is 15', () => {
-  const result = healthState({ name: 'Рыцарь', health: 15 });
-  expect(result).toBe('wounded');
+describe('healthState:', () => {
+  test('should return "healthy" if current health from 51 to 100', () => {
+    const result = healthState({ name: 'Маг', health: 90 });
+    expect(result).toBe('healthy');
+  });
+  test('should return "wounded" if current health from 15 to 50', () => {
+    const result = healthState({ name: 'лучник', health: 45 });
+    expect(result).toBe('wounded');
+  });
+  test('should return "critical" if current health from 0 to 14', () => {
+    const result = healthState({ name: 'Рыцарь', health: 14 });
+    expect(result).toBe('critical');
+  });
+  test('should return "wounded" if current health is 50', () => {
+    const result = healthState({ name: 'Рыцарь', health: 50 });
+    expect(result).toBe('wounded');
+  });
+  test('should return "wounded" if current health is 15', () => {
+    const result = healthState({ name: 'Рыцарь', health: 15 });
+    expect(result).toBe('wounded');
+  });
 });
